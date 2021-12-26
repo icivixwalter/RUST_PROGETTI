1828e0370c08b7e9cbea161b3f0f15b7496682c7
icivixwalter <47453186+icivixwalter@users.noreply.github.com>
2021-12-26 23:48:18 +0100

    tolto target 3

--- Capitolo_03/progetto_loop/target/debug/deps/progetto_loop-0ebdfc57a96222d0.d
+++
@@ -1,5 +0,0 @@
-c:\CASA\PROGRAMMI\RUST_PROGETTI\Capitolo_03\progetto_loop\target\debug\deps\progetto_loop-0ebdfc57a96222d0.rmeta: src\main.rs
-
-c:\CASA\PROGRAMMI\RUST_PROGETTI\Capitolo_03\progetto_loop\target\debug\deps\progetto_loop-0ebdfc57a96222d0.d: src\main.rs
-
-src\main.rs:
--- Capitolo_03/progetto_loop/target/debug/deps/progetto_loop.d
+++
@@ -1,5 +0,0 @@
-C:\CASA\PROGRAMMI\RUST_PROGETTI\Capitolo_03\progetto_loop\target\debug\deps\progetto_loop.exe: src\main.rs
-
-C:\CASA\PROGRAMMI\RUST_PROGETTI\Capitolo_03\progetto_loop\target\debug\deps\progetto_loop.d: src\main.rs
-
-src\main.rs:
